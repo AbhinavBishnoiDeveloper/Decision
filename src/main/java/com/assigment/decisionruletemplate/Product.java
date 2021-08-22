f9b74eb451bec75ea739fb3390434c3172f4a25b
rhpamAdmin <>
2021-08-22 23:09:03 +0530

    {/src/main/java/com/assigment/decisionruletemplate/Product.java}

--- src/main/java/com/assigment/decisionruletemplate/Product.java
+++ src/main/java/com/assigment/decisionruletemplate/Product.java
@@ -8,32 +8,7 @@ public class Product implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	private java.lang.String rere;
-
-	private java.lang.Float ffe;
-
 	public Product() {
-	}
-
-	public java.lang.String getRere() {
-		return this.rere;
-	}
-
-	public void setRere(java.lang.String rere) {
-		this.rere = rere;
-	}
-
-	public java.lang.Float getFfe() {
-		return this.ffe;
-	}
-
-	public void setFfe(java.lang.Float ffe) {
-		this.ffe = ffe;
-	}
-
-	public Product(java.lang.String rere, java.lang.Float ffe) {
-		this.rere = rere;
-		this.ffe = ffe;
 	}
 
 }
